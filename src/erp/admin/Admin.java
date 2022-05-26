1aa11d748b0c5c3cf4c79dea5ab959b9c1c12cbf
Kunal0007 <kunal.kv1dr64@gmail.com>
2022-05-26 09:57:54 +0530

    Admin

---
+++ src/erp/admin/Admin.java
@@ -0,0 +1,162 @@
+package erp.admin;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Window.Type;
+import java.awt.Color;
+import javax.swing.JTextField;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.ImageIcon;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.CardLayout;
+
+@SuppressWarnings("serial")
+public class Admin extends JFrame {
+
+	private JPanel contentPane;
+	private JPanel panel;
+	private JPanel panel_2;
+	private String name;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Admin frame = new Admin();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	
+	public String setname(String adminid) {
+		// TODO Auto-generated method stub
+		name = adminid;
+		return name;
+	}
+	
+	public Admin() {
+		setTitle("Admin");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(1047,650);
+		setLocationRelativeTo(null);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setLayout(null);
+		panel_1.setBackground(new Color(0, 139, 139, 220));
+		panel_1.setBounds(0, 11, 1031, 101);
+		contentPane.add(panel_1);
+		
+		JLabel lblNewLabel_1 = new JLabel("ADMIN");
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1.setFont(new Font("Poppins ExtraBold", Font.BOLD, 40));
+		lblNewLabel_1.setForeground(new Color(255, 255, 255));
+		lblNewLabel_1.setBounds(372, 11, 338, 79);
+		panel_1.add(lblNewLabel_1);
+		
+		panel = new JPanel();
+		panel.setBounds(0, 111, 302, 500);
+		panel.setBackground(new Color(23, 35, 51));
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		panel_2 = new JPanel();
+		panel_2.setBackground(new Color(23, 35, 51));
+		panel_2.setBounds(0, 0, 302, 110);
+		panel.add(panel_2);
+		panel_2.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon("F:\\eclipse\\ERP\\ERP System\\assets\\icons8-administrator-male-skin-type-7-48.png"));
+		lblNewLabel.setBounds(36, 34, 53, 43);
+		panel_2.add(lblNewLabel);
+		
+		JLabel lblNewLabel_2 = new JLabel("Hii, " + name);
+		lblNewLabel_2.setForeground(new Color(255, 255, 255));
+		lblNewLabel_2.setFont(new Font("Poppins Medium", Font.PLAIN, 18));
+		lblNewLabel_2.setBounds(99, 34, 164, 43);
+		panel_2.add(lblNewLabel_2);
+		
+		JPanel panel_3 = new JPanel();
+		panel_3.setBounds(302, 111, 729, 500);
+		contentPane.add(panel_3);
+		panel_3.setLayout(null);
+		
+		JButton btnNewButton = new JButton("Courses");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				panel_3.add(panel_4, "Courses");
+			}
+		});
+		btnNewButton.setFont(new Font("Poppins Medium", Font.PLAIN, 16));
+		btnNewButton.setForeground(new Color(255, 255, 255));
+		btnNewButton.setBackground(new Color(23, 35, 51));
+		btnNewButton.setBorderPainted(false);
+		btnNewButton.setBounds(0, 121, 302, 65);
+		panel.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Students Details");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				panel_3.add(panel_4_1, "Students Details");
+			}
+		});
+		btnNewButton_1.setFont(new Font("Poppins Medium", Font.PLAIN, 16));
+		btnNewButton_1.setForeground(new Color(255, 255, 255));
+		btnNewButton_1.setBackground(new Color(23, 35, 51));
+		btnNewButton_1.setBorderPainted(false);
+		btnNewButton_1.setBounds(0, 204, 302, 65);
+		panel.add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("Mark Attendance");
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton_2.setFont(new Font("Poppins Medium", Font.PLAIN, 16));
+		btnNewButton_2.setForeground(new Color(255, 255, 255));
+		btnNewButton_2.setBackground(new Color(23, 35, 51));
+		btnNewButton_2.setBorderPainted(false);
+		btnNewButton_2.setBounds(0, 289, 302, 65);
+		panel.add(btnNewButton_2);
+		
+		JButton btnNewButton_3 = new JButton("New button");
+		btnNewButton_3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		btnNewButton_3.setFont(new Font("Poppins Medium", Font.PLAIN, 16));
+		btnNewButton_3.setForeground(new Color(255, 255, 255));
+		btnNewButton_3.setBackground(new Color(23, 35, 51));
+		btnNewButton_3.setBorderPainted(false);
+		btnNewButton_3.setBounds(0, 377, 302, 65);
+		panel.add(btnNewButton_3);
+		
+		
+		
+	}
+
+	
+}
