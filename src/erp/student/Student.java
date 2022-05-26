1983a5281979744885c6b2e97883112542f03ab3
Kunal0007 <kunal.kv1dr64@gmail.com>
2022-05-26 09:59:17 +0530

    Student

---
+++ src/erp/student/Student.java
@@ -0,0 +1,44 @@
+package erp.student;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class Student extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Student frame = new Student();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Student() {
+		setTitle("Student");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(1047,650);
+		setLocationRelativeTo(null);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
