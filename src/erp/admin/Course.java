59fc073cc0309a67a89271e8ba13d38c5bcdc159
Kunal0007 <kunal.kv1dr64@gmail.com>
2022-05-29 23:02:17 +0530

    Course

---
+++ src/erp/admin/Course.java
@@ -0,0 +1,80 @@
+package erp.admin;
+
+import java.sql.*;
+
+
+import erp.database.DataBaseConnection;
+
+public class Course {
+	private String CourseCode;
+	private String CourseName;
+	private String SemesterYear;
+	
+	public void setCourseCode(String courseCode)
+	{
+		this.CourseCode = courseCode;
+	}
+	public void setCourseName(String courseName)
+	{
+		this.CourseName = courseName;
+	}
+	public void setSemesterYear(String semesterYear)
+	{
+		this.SemesterYear = semesterYear;
+	}
+	
+	public String getCourseCode()
+	{
+		return CourseCode;
+	}
+	public String getCourseName()
+	{
+		return CourseName;
+	}
+	public String getSemesterYear()
+	{
+		return SemesterYear;
+	}
+	
+	Connection con = DataBaseConnection.getConnection();
+	
+	public boolean insertCourseDetails()
+	{
+		
+		String query="insert into Courses (CourseCode, CourseName, SemesterorYear) values (?, ?, ?)";
+		try
+		{
+			PreparedStatement pStatement1 = con.prepareStatement(query);
+			pStatement1.setString(1, getCourseCode());
+			pStatement1.setString(2, getCourseName());
+			pStatement1.setString(3, getSemesterYear());
+			
+			pStatement1.execute();
+			
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+	
+	public boolean deleteCourseDetails(String courseCode)
+	{
+		String query1="delete from Courses where CourseCode = '" + courseCode + "'";
+		try
+		{
+			PreparedStatement pStatement1 = con.prepareStatement(query1);
+			pStatement1.execute();
+			
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+	
+}
